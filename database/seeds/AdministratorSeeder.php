8d23129feeae87bc552d0d8c9e3599ff9c3b4085
Yunju Lee <yunju@psu.edu>
2019-11-28 20:36:49 -0500

    Seeding administrator

---
+++ database/seeds/AdministratorSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        if (User::find(0))
+            return;
+        $admin = new User;
+        $admin->id = 0;
+        $admin->email = 'admin@dyq';
+        $admin->password = Hash::make('admin@dyq');
+        $admin->name = 'Administrator';
+        $admin->verified = true;
+        $admin->save();
+    }
+}
